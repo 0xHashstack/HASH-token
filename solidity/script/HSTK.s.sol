9f7cfa0f1b6bb0c81a93bf046814484302b89ad1
Saurabh <saurabh@hashstack.finance>
2024-10-21 11:40:27 +0530

    added scripts for deployment

--- solidity/script/HSTK.s.sol
+++ solidity/script/HSTK.s.sol
@@ -1,19 +1,18 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import {Script, console} from "forge-std/Script.sol";
-// import {Counter} from "../src/Counter.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {HstkToken} from "../src/HSTK.sol";
 
-// contract CounterScript is Script {
-//     Counter public counter;
+contract CounterScript is Script {
+    HstkToken public hashToken;
+    address admin = address(323);                              // Replace this with Address of the owner
+    function run() public {
 
-//     function setUp() public {}
-
-//     function run() public {
-//         vm.startBroadcast();
-
-//         counter = new Counter();
-
-//         vm.stopBroadcast();
-//     }
-// }
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        hashToken = new HstkToken(admin);
+        vm.label(address(hashToken),"HASH Token Address:");
+        vm.stopBroadcast();
+    }
+}
